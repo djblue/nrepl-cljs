ddaaa3075e52d59af489321b380cae69e095bae7
Chris Badahdah <a.s.badahdah@gmail.com>
2019-06-14 18:57:32 -0700

    Decoupled dispatch from sending back a value

--- src/nrepl/server.cljs
+++ src/nrepl/server.cljs
@@ -39,54 +39,48 @@
              (-> k namespace symbol))))
          dorun)))
 
-(defn dispatch [req send]
-  (let [id (:id req)
-        response
-        (case (:op req)
-          "clone"
-          (let [new-session (str (uuid/v4))]
-            (swap! sessions
-                   assoc
-                   new-session
-                   {:compiler (cljs.js/empty-state)})
-            {:new-session new-session
-             :status ["done"]})
-          "describe"
-          {:ops {:stacktrace 1}
-           :versions
-           {"nrepl"
-            {"major" 0 "minor" 2}}
-           "status" ["done"]}
-          "stacktrace"
-          {:name (.-name *e)
-           :class (.-name *e)
-           :method "test"
-           :message (.-message *e)
-           :status ["done"]}
-          "interrupt"
-          {:status ["done"]}
-          "eval"
-          (let [code (:code req)
-                session (:session req)
-                name-space (symbol (or (:ns req) "cljs.user"))
-                compiler (get-in @sessions [session :compiler])]
-            (try
-              (let [value (atom nil)
-                    out (with-out-str
-                          (reset! value (-> code read-string (nrepl-eval name-space))))]
-                {:out out
-                 :value @value
-                 :status ["done"]})
-              (catch js/Object e
-                (set! *e e)
-                {:err (.-stack e)
-                 :ex (pr-str e)
-                 :status ["done"]})))
-          "close"
-          {})]
-    (if (= (:op req) "close")
-      (send nil)
-      (send response))))
+(defn dispatch [req]
+  (case (:op req)
+    :clone
+    (let [new-session (str (uuid/v4))]
+      (swap! sessions
+             assoc
+             new-session
+             {:compiler (cljs.js/empty-state)})
+      {:new-session new-session})
+    :describe
+    {:ops {:stacktrace 1}
+     :versions
+     {"nrepl"
+      {"major" 0 "minor" 2}}}
+    :stacktrace
+    {:name (.-name *e)
+     :class (.-name *e)
+     :method "test"
+     :message (.-message *e)}
+    :interrupt {}
+    :eval
+    (let [code (:code req)
+          session (:session req)
+          name-space (symbol (or (:ns req) "cljs.user"))
+          compiler (get-in @sessions [session :compiler])]
+      (try
+        (let [value (atom nil)
+              out (with-out-str
+                    (reset! value (-> code read-string (nrepl-eval name-space))))]
+          {:out out
+           :value @value})
+        (catch js/Object e
+          (set! *e e)
+          {:err (.-stack e)
+           :ex (pr-str e)})))
+    :close
+    nil))
+
+(defn dispatch-send [req send]
+  (let [op (-> req :op keyword)]
+    (send (when-let [res (dispatch (assoc req :op op))]
+            (assoc res :status [:done])))))
 
 (defn promise? [v] (instance? js/Promise v))
 
@@ -115,7 +109,7 @@
 
 (defn transport [socket data]
   (let [[req] (decode (.toString data "utf8"))
-        handler (-> dispatch
+        handler (-> dispatch-send
                     attach-id
                     stringify-value
                     logger)]
