796b85e0a112b90a66a9cdab69423d64e7589563
Chris Badahdah <a.s.badahdah@gmail.com>
2019-06-15 12:26:00 -0700

    Improve proxy printing

--- src/nrepl/proxy.cljs
+++ src/nrepl/proxy.cljs
@@ -1,13 +1,19 @@
 (ns nrepl.proxy
   (:require [net :as net]
-            [nrepl.bencode :refer [encode decode]]
+            [nrepl.bencode :refer [encode decode-all]]
             [clojure.pprint :refer [pprint]]))
 
 (defn proxy-to [target direction]
-  (fn [data]
-    (pprint (assoc (first (decode (.toString data "utf8")))
-                   :direction direction))
-    (.write target data)))
+  (let [state (atom nil)]
+    (fn [data]
+      (.write target data)
+      (let [data (if (nil? @state)
+                   data
+                   (js/Buffer.concat (clj->js [@state data])))
+            [reqs data] (decode-all data :keywordize-keys true)]
+        (doseq [req reqs]
+          (pprint (assoc req :direction direction)))
+        (reset! state data)))))
 
 (defn handler [server target]
   (let [client (net/Socket.)]
