a472d80e32c86f0873288fe2a193f06524fa4667
Chris Badahdah <a.s.badahdah@gmail.com>
2020-06-27 14:33:59 -0700

    Fix issue with namespace handling

--- src/nrepl/repl.cljs
+++ src/nrepl/repl.cljs
@@ -8,27 +8,22 @@
 (defonce compile-state-ref (env/default-compiler-env))
 
 (defn eval [source ns]
-  (a/promise
-   [resolve]
-   (boot/init
-    compile-state-ref
-    {}
-    #(cljs/eval-str
+  (let [ns (symbol ns)]
+    (a/promise
+     [resolve]
+     (boot/init
       compile-state-ref
-      source
-      "<repl>"
-      {:eval          cljs/js-eval
-       :ns            ns
-       :target        :nodejs
-       :context       :expr
-       :def-emits-var true
-       :load          (fn [name cb]
-                        (println :args name cb)
-                        (boot/load
-                         compile-state-ref
-                         name
-                         (fn [result]
-                           (println :result result)
-                           (cb %))))}
-
-      resolve))))
+      {:path "target/bootstrap"}
+      #(cljs/eval-str
+        compile-state-ref
+        source
+        "<repl>"
+        {:eval          cljs/js-eval
+         :ns            ns
+         :target        :nodejs
+         ;:verbose       true
+         :context       :expr
+         :analyze-deps  false
+         :def-emits-var true
+         :load (partial boot/load compile-state-ref)}
+        resolve)))))
